b7679c7ee59304722e8b7ff8657df00d7e449e4b
shripada <shripada.hebbar@codecraft.co.in>
2025-09-19 11:51:03 +0530

    implemented square root function

--- operations.ts
+++ operations.ts
@@ -6,8 +6,16 @@ function subtract(a: number, b: number): number {
   return a - b;
 }
 
-
 function multiply(a: number, b: number) {
   return a * b;
 }
 
+// We need to document each function
+/**
+ * Finds the square root of the number passed. And number should be positive
+ * @param num
+ * @returns
+ */
+function sqrt(num: number): number {
+  return Math.sqrt(num);
+}
