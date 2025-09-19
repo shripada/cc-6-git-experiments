c5d64bef9bcbb47eb3ffe8a5a1addc61efd6b165
shripada <shripada.hebbar@codecraft.co.in>
2025-09-19 12:23:00 +0530

    implemented divide

--- operations.ts
+++ operations.ts
@@ -10,7 +10,6 @@ function multiply(a: number, b: number) {
   return a * b;
 }
 
-// We need to document each function
 /**
  * Finds the square root of the number passed. And number should be positive
  * @param num
@@ -18,4 +17,17 @@ function multiply(a: number, b: number) {
  */
 function sqrt(num: number): number {
   return Math.sqrt(num);
+}
+
+/**
+ * Divides a by b and returns the quotient
+ * @param a the divident
+ * @param b the divisor, must be non zero
+ * @returns quotient. Throws an exception if b is zero
+ */
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('divisor can not be zero!');
+  }
+  return a / b;
 }
